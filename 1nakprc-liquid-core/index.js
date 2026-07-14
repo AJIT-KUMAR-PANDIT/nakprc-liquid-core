069d99d7c660b0c434ce68b1ed03815c9bc8497f
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2026-07-15 03:13:56 +0530

    q

--- 1nakprc-liquid-core/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Nakprc Liquid Core - npm package entry
- * Exporting liquid physics and optics for component libraries.
- */
-
-export { injectNakprcStyles } from './src/styles.js';
-export { NakprcLiquidLens } from './src/lens.js';
-export { NakprcLiquidBlob } from './src/blob.js';
-export { initNakprcDOM } from './src/init.js';
